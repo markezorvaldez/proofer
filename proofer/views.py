4e128e3f55f61828589a3070b60bdd92434416af
Mark <mark.valdez12@imperial.ac.uk>
2017-01-04 21:02:34 +0000

    Added textbox for premise and proof.

--- proofer/views.py
+++ proofer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Proof
+from .forms import ProofForm
 
 def post_list(request):
-	proofs = Proof.objects.all()
+
+	proofs = ProofForm()
 	return render(request, 'proofer/post_list.html', {'proofs': proofs})
