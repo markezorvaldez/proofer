16d3456c376fc9a6fb076f43a60c803fc9265bc4
Mark <mark.valdez12@imperial.ac.uk>
2017-01-04 22:01:35 +0000

    Little code testing to know how to append texts in form textbox.

--- proofer/views.py
+++ proofer/views.py
@@ -3,6 +3,14 @@ from .models import Proof
 from .forms import ProofForm
 
 def post_list(request):
-
-	proofs = ProofForm()
+	if request.method == "POST":
+		proofs = ProofForm(request.POST)
+		if proofs.is_valid():
+			proof = proofs.save(commit=False)
+			proof.premise += " el kappa lul jebaited"
+			#proof.proof = proofs.proof
+			proof.save()
+			proofs = ProofForm(instance=proof)
+	else:
+		proofs = ProofForm()
 	return render(request, 'proofer/post_list.html', {'proofs': proofs})
