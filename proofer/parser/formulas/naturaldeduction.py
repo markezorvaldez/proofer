b00d5c90cfddd2a2cd9485d5c6bacd04e60fcc1a
Mark <mark.valdez12@imperial.ac.uk>
2017-01-17 20:46:07 +0000

    Cleaned commented code. Added failing test cases.

--- proofer/parser/formulas/naturaldeduction.py
+++ proofer/parser/formulas/naturaldeduction.py
@@ -7,7 +7,7 @@ if sys.version_info[0] >= 3:
 
 
 class Formula:
-
+	"""Formula object representing an atom through a character."""
 
 	def __init__(self, formula):
 		self.formula = formula
@@ -16,16 +16,9 @@ class Formula:
 		return self.formula
 
 	def __eq__(self, other):
-		print(self.__str__() + " is equal to " + other.__str__())
 		return self.__str__() == other.__str__()
-		# return self.canProve(other)
 
 	def infers(self, formula):
-		# print("canProve")
-		# print(self)
-		# print(formula)
-		# print("self: " + self.__str__())
-		# print("other: " + formula.__str__())
 		return self.__str__() == formula.__str__()
 
 class AndFormula(Formula):
@@ -49,87 +42,21 @@ class AndFormula(Formula):
 				for rightFormula in right:
 					left.remove(rightFormula)
 			except ValueError:
-				# print("Value error")
 				return False
-			# print(left)
 			return not left
-			# print(self.__str__() + " equal " + other.__str__())
-			# print(self.formulas)
-			# print(other.formulas)
 
-			# result = left == right
-			# print(result)
-			# return result
 		except AttributeError:
-			# print("Attribute error")
 			return False
-
-		# print(self.__str__() + " equal " + other.__str__())
-		# try:
-		# 	result = set(self.formulas) == set(other.formulas) #lists are not hashable so i cannot do dis
-
-		# 	print("SAME TYPE BUT RESULT IS " + result)
-		# 	return result
-		# except TypeError:
-		# 	print("TypeError")
-		# 	print(self.__str__() + " type is " + str(type(self)))
-		# 	print(other.__str__() + " type is " + str(type(other)))
-
-		# 	return False
-
-	# def canProve(self, formula):
-		# print("ASDL;KFJAL;SKDFJLKASDJFAL;KSDJF;ALSDF")
-		# this is being called when infers is being called
-		# result = False
-		# for f in self.formulas:
-			# result = result or f.canProve(formula)
-		# return result
 
 	def infers(self, formula):
 		return formula in self.eliminationList()
 
 	def eliminationList(self):
-		# A*B*C*D -> (A, B, C, A*B, A*C, A*D, B*C, B*D, C*D, A*B*C, A*B*D,
-		#             A*C*D, B*C*D)
-		# print("eliminating " + self.__str__())
 		result = self.formulas[:]
+		result.append(self)
 		max_len = len(self.formulas)
-		# print([s.__str__() for s in result])
 		for L in range(2, max_len):
-			# print("length at the moment is " + str(L))
 			for f in combinations(self.formulas, L):
-				# print("length of formulas is " + str(len(self.formulas)))
-				# print("value of maxlen is " + str(max_len))
 				result.append(AndFormula(*f))
-			# print([s.__str__() for s in result])
-		# print("hello")
+
 		return result
-
-
-# formulaA = Formula("A")
-# formulaB = Formula("B")
-# formulaC = Formula("C")
-# formulaD = Formula("D")
-# formulaE = Formula("E")
-# abcd = AndFormula(formulaA, formulaB, formulaC, formulaD)
-# abc = AndFormula(formulaA, formulaB, formulaC)
-# ab = AndFormula(formulaA, formulaB)
-# ba = AndFormula(formulaB, formulaA)
-# bca = AndFormula(formulaB, formulaC, formulaA)
-# ae = AndFormula(formulaA, formulaE)
-# print(abcd.canProve(formulaA))
-# # print(abcd)
-# # print(ab)
-# # print(abcd.canProve(ab))
-# # elim = abcd.eliminationList()
-# # print(len(elim))
-# # print(ab in elim)
-# # print(ba in elim)
-# # print(bca in elim)
-# # print(abcd in elim)
-# # print(ae in elim) # should be false but true
-# # print(formulaE in elim)
-# # print([f.__str__() for f in elim])
-# # print(elim)
-# print(ae == formulaA)
-# print(formulaA == ae)
--- proofer/parser/formulas/test.py
+++ proofer/parser/formulas/test.py
@@ -22,7 +22,7 @@ def testAndFormulaInfers2():
 	ab = nat.AndFormula(a, b)
 	abc = nat.AndFormula(a, b, c)
 
-	print("A * B * C infers A * B?")
+	print("ABC infers AB?")
 	print(abc.infers(ab))
 
 def testAndFormulaInfers3():
@@ -32,7 +32,7 @@ def testAndFormulaInfers3():
 	ba = nat.AndFormula(b, a)
 	abc = nat.AndFormula(a, b, c)
 
-	print("A * B * C infers B * A?")
+	print("ABC infers BA?")
 	print(abc.infers(ba))
 
 def testAndFormulaInfers4():
@@ -41,15 +41,73 @@ def testAndFormulaInfers4():
 	c = nat.Formula("C")
 	ab = nat.AndFormula(a, b)
 	ac = nat.AndFormula(a, c)
+	ca = nat.AndFormula(c, a)
 
 	print("AB infers AC?")
 	print(ab.infers(ac))
-	# print(ac.infers(ab))
-	print([f.__str__() for f in ab.eliminationList()])
-	print([f.__str__() for f in ac.eliminationList()])
+	print("AC infers AB?")
+	print(ac.infers(ab))
+	print("AC infers AC?")
+	print(ac.infers(ac))
+	print("AC infers CA?")
+	print(ac.infers(ca))
+
+
+def testAndFormula():
+	a = nat.Formula("A")
+	b = nat.Formula("B")
+	c = nat.Formula("C")
+	d = nat.Formula("D")
+	ab = nat.AndFormula(a, b)
+	ba = nat.AndFormula(b, a)
+	ac = nat.AndFormula(a, c)
+	ca = nat.AndFormula(c, a)
+	ad = nat.AndFormula(a, d)
+	bc = nat.AndFormula(b, c)
+	cd = nat.AndFormula(c, d)
+	abc = nat.AndFormula(a, b, c)
+	abcd1 = nat.AndFormula(a, b, c, d)
+	abcd2 = nat.AndFormula(ab, cd)
+	abcd3 = nat.AndFormula(abc, d)
+
+	print("AB infers A?")
+	print(ab.infers(a))
+
+	print("ABC infers AB?")
+	print(abc.infers(ab))
+
+	print("ABC infers BA?")
+	print(abc.infers(ba))
+
+	print("AB infers AC?")
+	print(ab.infers(ac))
+
+	print("AC infers AB?")
+	print(ac.infers(ab))
+
+	print("AC infers AC?")
+	print(ac.infers(ac))
+
+	print("AC infers CA?")
+	print(ac.infers(ca))
+
+	print("ABC infers ABCD?")
+	print(abc.infers(abcd1))
+
+	print("AB * CD infers ABCD?")
+	print(abcd2.infers(abcd1))
+
+	print("ABC * D infers ABCD?")
+	print(abcd3.infers(abcd1))
+
+	print("AB * CD infers ABC * D?")
+	print(abcd2.infers(abcd3))
+
+
 
 print("---------------------------     TEST     ------------------------")
-testAndFormulaInfers1()
-testAndFormulaInfers2()
-testAndFormulaInfers3()
-testAndFormulaInfers4()
+# testAndFormulaInfers1()
+# testAndFormulaInfers2()
+# testAndFormulaInfers3()
+# testAndFormulaInfers4()
+testAndFormula()
