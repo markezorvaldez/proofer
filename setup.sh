30ffc6f52d49049b6767cffcd60ae9670996bb47
Mark <mark.valdez12@imperial.ac.uk>
2017-06-23 16:10:59 +0100

    change setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ echo `sudo apt install virtualenv`
 echo `virtualenv --python=python3.5 myvenv`
 echo `source myvenv/bin/activate`
 echo `pip install -r requirements.txt`
+echo `python manage.py migrate`
